3a73664d6ae9cf9f3cab64ccdd9e15bfb343a20a
darzu <daryl.zuniga@gmail.com>
2020-02-22 11:43:04 -0800

    iter mut

--- src/second.rs
+++ src/second.rs
@@ -62,6 +62,7 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
+// MY IterMut
 // pub struct IterMut<T> {
 //     nx: Link<T>,
 // }
@@ -95,9 +96,43 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_mut().map(|node| &mut **node),
+        }
+    }
+}
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+    }
 
     #[test]
     fn iter() {
